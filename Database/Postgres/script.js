afd41bc55ffdd10fd1bfe7ad1b9204c89bb41986
Lawrence Malloy <lcmalloy@gmail.com>
2022-04-26 22:08:07 -0700

    dded newrelic to gitignore

--- Database/Postgres/script.js
+++ Database/Postgres/script.js
@@ -4,27 +4,21 @@ import http from 'k6/http';
 import { sleep } from 'k6';
 
 export const options = {
+  insecureSkipTLSVerify: true,
+  noConnectionReuse: false,
   stages: [
-    { duration: '2m', target: 100 }, // below normal load
-    { duration: '5m', target: 100 },
-    { duration: '2m', target: 200 }, // normal load
-    { duration: '5m', target: 200 },
-    { duration: '2m', target: 300 }, // around the breaking point
-    { duration: '5m', target: 300 },
-    { duration: '2m', target: 400 }, // beyond the breaking point
-    { duration: '5m', target: 400 },
-    { duration: '10m', target: 0 }, // scale down. Recovery stage.
-  ],
+    { duration: '15s', target: 100 },
+    { duration: '30s', target: 100 },
+    { duration: '60s', target: 0 }
+  ]
 };
 
 export default function () {
   const BASE_URL = 'http://localhost:3030'; // make sure this is not production
 
   const responses = http.batch([
-    ['GET', `${BASE_URL}/product?product_id=1000011`],
-    ['GET', `${BASE_URL}/productStyle?product_id=1000011`],
-    ['GET', `${BASE_URL}/relatedProduct?product_id=1000011`],
+    ['GET', `${BASE_URL}/product?product_id=${Math.floor(Math.random() * 1000011)}`],
   ]);
 
-  sleep(1);
+  sleep(0.1);
 }
